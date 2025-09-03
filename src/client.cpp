0780ca16dafddeae15f22625411055098dbe50a1
Nias26 <genangeliludovico26@gmail.com>
2025-09-03 17:22:54 +0200

    feat: encrypt traffic with SSL TLS

--- src/client.cpp
+++ src/client.cpp
@@ -1,82 +1,153 @@
 #include <arpa/inet.h>
 #include <cstring>
+#include <filesystem>
 #include <future>
 #include <iostream>
-#include <memory>
 #include <mutex>
 #include <netinet/in.h>
-#include <string>
+#include <openssl/bio.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <optional>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #define BUF_SIZE 4096
 #define IP_ADDR "192.168.1.2"
 
-static std::mutex lock;
+void InitializeSSL() {
+  SSL_load_error_strings();
+  SSL_library_init();
+  OpenSSL_add_all_algorithms();
+}
 
-int receive_bytes(std::shared_ptr<int> sockfd) {
+void DestroySSL() {
+  ERR_free_strings();
+  EVP_cleanup();
+}
+
+void ShutdownSSL(SSL *cSSL) {
+  SSL_shutdown(cSSL);
+  SSL_free(cSSL);
+}
+
+int receive_bytes(int sockfd, SSL *cSSl) {
   int ok;
   char buf[BUF_SIZE]{0};
 
-  while (true) {
-    memset(buf, 0, sizeof(buf));
-
-    ok = recv(*sockfd, buf, sizeof(buf), 0);
-    if (*sockfd == -1) {
-      std::cerr << "[ERR] recv(): " << strerror(errno) << std::endl;
-      close(*sockfd);
-      return -1;
-    }
-
-    if (ok == 0) {
-      std::cout << "Server disconnected. Return." << std::endl;
-      break;
-    }
-
-    std::cout << "\nserver]: " << buf << std::endl;
-  }
-  return ok;
-}
-
-int send_bytes(std::shared_ptr<int> sockfd) {
-  int ok;
-  std::string line;
-
-  while (true) {
-    line.erase();
-
-    std::getline(std::cin, line);
-    if (line.length() == 0)
-      continue;
-
-    lock.lock();
-    int ok = send(*sockfd, line.c_str(), line.size(), 0);
-    if (ok == -1) {
-      std::cerr << "[ERR] send(): " << strerror(errno) << std::endl;
-      close(*sockfd);
-      return -1;
-    }
-    lock.unlock();
-  }
-  return ok;
-}
-
-int main(void) {
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in addr = {AF_INET, htons(8080), {inet_addr(IP_ADDR)}};
-  int ok = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-  if (ok == -1) {
-    std::cerr << "[ERR] connect(): " << strerror(errno) << std::endl;
-    close(sock);
+  memset(buf, 0, sizeof(buf));
+  ok = SSL_read(cSSl, buf, sizeof(buf));
+  std::cout << "Bytes read: " << ok << std::endl;
+  if (ok < 0) {
+    std::cerr << "[ERR] SSL_read(): "
+              << ERR_error_string(SSL_get_error(cSSl, ok), nullptr)
+              << std::endl;
+    perror("sys: [ERR] recv(): ");
     return -1;
   }
 
-  std::shared_ptr<int> sh_sock = std::make_shared<int>(sock);
+  if (ok == 0) {
+    std::cout << "Server disconnected. Return." << std::endl;
+    return 0;
+  }
 
-  auto t_send = std::async(std::launch::async, send_bytes, sh_sock);
-  auto t_recv = std::async(std::launch::async, receive_bytes, sh_sock);
-  t_recv.wait();
+  if (ok > 0) {
+    std::string str{buf};
+    std::cout << "\nserver]: " << str << std::endl;
+  }
+  return ok;
+}
 
+int send_bytes(int sockfd, SSL *cSSl) {
+  int ok;
+  std::string line;
+
+  line.erase();
+  std::getline(std::cin, line);
+  if (line.length() == 0)
+    return 0;
+
+  ok = SSL_write(cSSl, line.c_str(), line.size());
+  std::cout << "Sent " << ok << " bytes" << std::endl;
+  if (ok < 0) {
+    std::cerr << "[ERR] SSL_write(): "
+              << ERR_error_string(SSL_get_error(cSSl, ok), nullptr)
+              << std::endl;
+    perror("sys: [ERR] send(): ");
+    return -1;
+  }
+  return ok;
+}
+
+std::optional<std::filesystem::path> get_cert(const std::string dirname,
+                                              const std::string &filename) {
+  using namespace std;
+  filesystem::path folder(dirname);
+  if (!filesystem::exists(dirname) || !filesystem::is_directory(dirname)) {
+    std::cerr << "[ERR] get_cert(): Bad dirname" << std::endl;
+    return std::nullopt;
+  }
+
+  filesystem::path filepath = folder / filename;
+  if (filesystem::exists(filepath) && filesystem::is_regular_file(filepath)) {
+    return filepath;
+  } else {
+    std::cerr << "[ERR] get_cert(): Bad filename" << std::endl;
+    return std::nullopt;
+  }
+}
+
+int main(void) {
+  // Initialize SSL envs
+  InitializeSSL();
+  auto cert = get_cert("./cert", "cert.pem");
+  auto key = get_cert("./cert", "key.pem");
+
+  // Create socket and connect to server
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in addr = {AF_INET, htons(8080), {inet_addr(IP_ADDR)}};
+  connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+  // Create SSL Context
+  SSL_CTX *sslctx = SSL_CTX_new(TLS_client_method());
+  SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
+  SSL_CTX_use_certificate_file(sslctx, cert->c_str(), SSL_FILETYPE_PEM);
+  SSL_CTX_use_PrivateKey_file(sslctx, key->c_str(), SSL_FILETYPE_PEM);
+
+  // Initialize SSL connection
+  SSL *cSSl = SSL_new(sslctx);
+  SSL_set_fd(cSSl, sock);
+  SSL_connect(cSSl);
+  SSL_accept(cSSl);
+
+  // Send and receive messages ( TODO: Send https requests)
+  // auto t_recv = std::async(std::launch::async, receive_bytes, sock, cSSl);
+  // auto t_send = std::async(std::launch::async, send_bytes, sock, cSSl);
+  // t_recv.wait();
+
+  struct pollfd fds[2]{{0, POLLIN, 0}, {sock, POLLIN, 0}};
+
+  int ok;
+  while (true) {
+    poll(fds, 2, 5000);
+
+    if (fds[0].revents & POLLIN) {
+      ok = send_bytes(sock, cSSl);
+      if (ok <= 0)
+        break;
+    }
+    if (fds[1].revents & POLLIN) {
+      ok = receive_bytes(sock, cSSl);
+      if (ok <= 0)
+        break;
+    }
+  }
+
+  // Deallocate structures and sockets
+  DestroySSL();
+  ShutdownSSL(cSSl);
   close(sock);
   return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,15 +1,18 @@
-/*
- * TODO: Add OpenSSL Security Layer to encrypt connection
- */
-
 #include <arpa/inet.h>
-#include <cstddef>
+#include <cstdio>
 #include <cstring>
+#include <filesystem>
 #include <future>
 #include <iostream>
-#include <memory>
 #include <mutex>
 #include <netinet/in.h>
+#include <openssl/bio.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <optional>
+#include <string>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -18,94 +21,142 @@
 
 std::mutex lock;
 
-int receive_bytes(std::shared_ptr<int> sockfd, std::shared_ptr<int> clientfd) {
+void InitializeSSL() {
+  SSL_load_error_strings();
+  SSL_library_init();
+  OpenSSL_add_all_algorithms();
+}
+
+void DestroySSL() {
+  ERR_free_strings();
+  EVP_cleanup();
+}
+
+void ShutdownSSL(SSL *cSSL) {
+  SSL_shutdown(cSSL);
+  SSL_free(cSSL);
+}
+
+int receive_bytes(int sockfd, SSL *cSSl) {
   int ok;
   char buf[BUF_SIZE]{0};
 
-  while (true) {
-    memset(buf, 0, sizeof(buf));
+  memset(buf, 0, sizeof(buf));
+  ok = SSL_read(cSSl, buf, sizeof(buf));
+  std::cout << "Bytes read: " << ok << std::endl;
+  if (ok < 0) {
+    std::cerr << "[ERR] SSL_read(): "
+              << ERR_error_string(SSL_get_error(cSSl, ok), nullptr)
+              << std::endl;
+    perror("sys: [ERR] recv(): ");
+    return -1;
+  }
 
-    ok = recv(*clientfd, buf, sizeof(buf), 0);
+  if (ok == 0) {
+    std::cout << "Client disconnected. Return." << std::endl;
+    return 0;
+  }
 
-    if (*sockfd == -1) {
-      std::cerr << "[ERR] recv(): " << strerror(errno) << std::endl;
-      close(*clientfd);
-      close(*sockfd);
-      return -1;
-    }
-
-    if (ok == 0) {
-      std::cout << "Client disconnecter. Return." << std::endl;
-      break;
-    }
-
-    std::cout << "\nclient]: " << buf << std::endl;
+  if (ok > 0) {
+    std::string str{buf};
+    std::cout << "\nclient]: " << str << std::endl;
   }
   return ok;
 }
 
-int send_bytes(std::shared_ptr<int> sockfd, std::shared_ptr<int> clientfd) {
+int send_bytes(int sockfd, SSL *cSSl) {
   int ok;
   std::string line;
 
-  while (true) {
-    line.erase();
+  line.erase();
+  std::getline(std::cin, line);
+  if (line.length() == 0)
+    return 0;
 
-    std::getline(std::cin, line);
-    if (line.length() == 0)
-      continue;
-
-    lock.lock();
-    int ok = send(*clientfd, line.c_str(), line.size(), 0);
-    if (ok == -1) {
-      std::cerr << "[ERR] send(): " << strerror(errno) << std::endl;
-      close(*clientfd);
-      close(*sockfd);
-      return -1;
-    }
-    lock.unlock();
+  ok = SSL_write(cSSl, line.c_str(), line.size());
+  std::cout << "Sent " << ok << " bytes" << std::endl;
+  if (ok < 0) {
+    std::cerr << "[ERR] SSL_send(): "
+              << ERR_error_string(SSL_get_error(cSSl, ok), nullptr)
+              << std::endl;
+    perror("sys: [ERR] send(): ");
+    return -1;
   }
   return ok;
 }
 
+std::optional<std::filesystem::path> get_cert(const std::string dirname,
+                                              const std::string &filename) {
+  using namespace std;
+  filesystem::path folder(dirname);
+  if (!filesystem::exists(dirname) || !filesystem::is_directory(dirname)) {
+    std::cerr << "[ERR] get_cert(): Bad dirname" << std::endl;
+    return std::nullopt;
+  }
+
+  filesystem::path filepath = folder / filename;
+  if (filesystem::exists(filepath) && filesystem::is_regular_file(filepath)) {
+    return filepath;
+  } else {
+    std::cerr << "[ERR] get_cert(): Bad filename" << std::endl;
+    return std::nullopt;
+  }
+}
+
 int main(void) {
+  // Initialize SSL envs
+  InitializeSSL();
+  auto cert = get_cert("./cert", "cert.pem");
+  auto key = get_cert("./cert", "key.pem");
+
+  // Create socket and listen for connections
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   struct sockaddr_in addr = {AF_INET, htons(8080), {inet_addr(IP_ADDR)}};
-  int ok = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-  if (ok == -1) {
-    std::cerr << "[ERR] bind(): " << strerror(errno) << std::endl;
-    close(sock);
-    return -1;
-  }
+  bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+  listen(sock, 1);
 
-  ok = listen(sock, 1);
-  if (ok == -1) {
-    std::cerr << "[ERR] listen(): " << strerror(errno) << std::endl;
-    close(sock);
-    return -1;
-  }
-
+  // Log
   char ip_str[INET_ADDRSTRLEN];
   inet_ntop(AF_INET, &addr.sin_addr, ip_str, sizeof(ip_str));
+  std::cout << "Listening at " << ip_str << " on port " << ntohs(addr.sin_port)
+            << std::endl;
 
-  std::cout << "Listening at " << ip_str << " on port " << ntohs(addr.sin_port);
+  // Create SSL Context
+  SSL_CTX *sslctx = SSL_CTX_new(TLS_server_method());
+  SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
+  SSL_CTX_use_certificate_file(sslctx, cert->c_str(), SSL_FILETYPE_PEM);
+  SSL_CTX_use_PrivateKey_file(sslctx, key->c_str(), SSL_FILETYPE_PEM);
 
-  int client_fd = accept(sock, NULL, NULL);
-  if (client_fd == -1) {
-    std::cerr << "[ERR] accept(): " << strerror(errno) << std::endl;
-    close(sock);
-    return -1;
+  // Get client file descriptor
+  int client_fd = accept(sock, nullptr, nullptr);
+  SSL *cSSl = SSL_new(sslctx);
+  SSL_set_fd(cSSl, client_fd);
+  SSL_accept(cSSl);
+
+  const char *ping = "Hello";
+  SSL_write(cSSl, ping, strlen(ping));
+
+  struct pollfd fds[2]{{0, POLLIN, 0}, {client_fd, POLLIN, 0}};
+
+  int ok;
+  while (true) {
+    poll(fds, 2, 5000);
+
+    if (fds[0].revents & POLLIN) {
+      ok = send_bytes(sock, cSSl);
+      if (ok <= 0)
+        break;
+    }
+    if (fds[1].revents & POLLIN) {
+      ok = receive_bytes(sock, cSSl);
+      if (ok <= 0)
+        break;
+    }
   }
 
-  std::shared_ptr<int> sh_sock = std::make_shared<int>(sock);
-  std::shared_ptr<int> sh_client_fd = std::make_shared<int>(client_fd);
-
-  auto t_recv =
-      std::async(std::launch::async, receive_bytes, sh_sock, sh_client_fd);
-  auto t_send =
-      std::async(std::launch::async, send_bytes, sh_sock, sh_client_fd);
-  t_recv.wait();
-
+  // Deallocate structures and sockets
+  DestroySSL();
+  ShutdownSSL(cSSl);
   close(client_fd);
   close(sock);
   return 0;
